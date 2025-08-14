97db857108fc1691fdadd26f4264e3bea2a22fd0
fd <fdecunta@agro.uba.ar>
2025-08-14 15:49:06 -0300

    Add sql schema

--- init_db.sql
+++ init_db.sql
@@ -10,7 +10,13 @@ CREATE TABLE IF NOT EXISTS papers (
     source TEXT
 );
 
-CREATE TABLE IF NOT EXISTS screener_outputs (
+CREATE TABLE IF NOT EXISTS criteria (
+    criteria_id INTEGER PRIMARY KEY,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    text TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS screener (
     suggestion_id INTEGER PRIMARY KEY,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     criteria_id INTEGER NOT NULL,
@@ -20,10 +26,4 @@ CREATE TABLE IF NOT EXISTS screener_outputs (
     human_validated INTEGER NOT NULL DEFAULT 0 CHECK (human_validated IN (0, 1)),
     FOREIGN KEY (paper_id) REFERENCES papers (paper_id),
     FOREIGN KEY (criteria_id) REFERENCES criteria (criteria_id)
-);
-
-CREATE TABLE IF NOT EXISTS criteria (
-    criteria_id INTEGER PRIMARY KEY,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    text TEXT NOT NULL
 );
