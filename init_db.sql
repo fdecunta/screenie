fcd310674560bdceebb0cf057e48c10bc38f09ca
fd <fdecunta@agro.uba.ar>
2025-08-18 15:39:15 -0300

    change db schema

--- init_db.sql
+++ init_db.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS papers (
     paper_id INTEGER PRIMARY KEY,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    title TEXT NOT NULL,
+    title TEXT NOT NULL UNIQUE,
     authors TEXT NOT NULL, 
     year INTEGER NOT NULL,
-    abstract TEXT NOT NULL,
-    url TEXT NOT NULL,
-    doi TEXT,
+    abstract TEXT NOT NULL UNIQUE,
+    url TEXT NOT NULL UNIQUE,
+    doi TEXT UNIQUE,
     source TEXT
 );
 
@@ -24,6 +24,8 @@ CREATE TABLE IF NOT EXISTS screener (
     veredict INTEGER NOT NULL CHECK (veredict IN (-1, 0, 1)),
     reason TEXT NOT NULL,
     human_validated INTEGER NOT NULL DEFAULT 0 CHECK (human_validated IN (0, 1)),
+    input_tokens INTEGER NOT NULL,
+    output_tokens INTEGER NOT NULL,
     FOREIGN KEY (paper_id) REFERENCES papers (paper_id),
     FOREIGN KEY (criteria_id) REFERENCES criteria (criteria_id)
 );
