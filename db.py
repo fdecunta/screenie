cc85c578d5d0c4cedc6327b9e7748f46595c75f1
Facundo Decunta <fdecunta@agro.uba.ar>
2025-08-18 22:30:04 -0300

    move insert_file to db module

--- db.py
+++ db.py
@@ -1,4 +1,7 @@
+import click
+import os
 import sqlite3
+import sys
 
 def init_db(db_name: str, sql_file="schema.sql"):
     conn = sqlite3.connect(db_name)
@@ -19,3 +22,36 @@ def init_db(db_name: str, sql_file="schema.sql"):
     conn.close()
 
     return success
+
+
+def insert_file(db_path: str, file_path: str):
+    filename = os.path.basename(file_path)
+
+    fmt_filename = click.format_filename(filename)
+    fmt_database = click.format_filename(os.path.basename(db_path))
+
+    # read file as bytes
+    with open(file_path, "rb") as f:
+        file_bytes = f.read()
+
+    # use context manager for connection
+    with sqlite3.connect(db_path) as con:
+        cur = con.cursor()
+        try:
+            cur.execute("""
+                INSERT INTO input_files (name, content)
+                VALUES (?, ?)
+            """, (filename, file_bytes))
+            con.commit()
+            click.secho(f"Added {fmt_filename} to {fmt_database}", fg="green")
+        except sqlite3.IntegrityError as e:
+            msg = str(e)
+            if "UNIQUE constraint" in msg:
+                click.secho(f"File {fmt_filename} already exists in the database.", fg="red", err=True)
+            else:
+                click.secho(f"Failed to import {fmt_filename}: {msg}", fg="red", err=True)
+            sys.exit(1)
+        except sqlite3.DatabaseError as e:
+            click.secho(f"Database error: {e}", fg="red", err=True)
+            sys.exit(1)
+
--- reader.py
+++ reader.py
@@ -6,6 +6,7 @@ import sqlite3
 import sys
 from typing import Optional, List
 import unicodedata
+import db
 
 
 class Paper(BaseModel):
@@ -50,42 +51,11 @@ def validate_papers(papers: List[dict]) -> List[Paper]:
     return valid_papers
 
 
-def insert_file(db_path, file_path):
-    filename = os.path.basename(file_path)
-
-    fmt_filename = click.format_filename(filename)
-    fmt_database = click.format_filename(os.path.basename(db_path))
-
-    # read file as bytes
-    with open(file_path, "rb") as f:
-        file_bytes = f.read()
-
-    # use context manager for connection
-    with sqlite3.connect(db_path) as con:
-        cur = con.cursor()
-        try:
-            cur.execute("""
-                INSERT INTO input_files (name, content)
-                VALUES (?, ?)
-            """, (filename, file_bytes))
-            con.commit()
-            click.secho(f"Added {fmt_filename} to {fmt_database}", fg="green")
-        except sqlite3.IntegrityError as e:
-            msg = str(e)
-            if "UNIQUE constraint" in msg:
-                click.secho(f"File {fmt_filename} already exists in the database.", fg="red", err=True)
-            else:
-                click.secho(f"Failed to import {fmt_filename}: {msg}", fg="red", err=True)
-            sys.exit(1)
-        except sqlite3.DatabaseError as e:
-            click.secho(f"Database error: {e}", fg="red", err=True)
-            sys.exit(1)
-
 
 def import_from_bib(db_path: str, file_path: str):
 #    bib_database = read_bib(path)
 #    papers_list = validate_papers(bib_database.entries)
 
-    insert_file(db_path, file_path)
+    db.insert_file(db_path, file_path)
 
 
